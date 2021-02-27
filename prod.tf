21961f94a9dff5a0f0ebaa8a7ee43d76501ea98b
Vedran Brnjetic <vedran.brnjetic@metropolia.fi>
2021-02-27 20:02:21 +0200

    adopt default vpc and rename tf file

--- prod.tf
+++ prod.tf
@@ -3,7 +3,9 @@ provider "aws" {
   region  = "eu-north-1"
 }
 
-resource "aws_s3_bucket" "tf_course" {
+resource "aws_s3_bucket" "prod_tf_bucket" {
   bucket = "croviking-terraform-learning"
   acl    = "private"
 }
+
+resource "aws_default_vpc" "default" {}
